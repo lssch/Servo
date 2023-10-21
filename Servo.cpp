4f5cdde243fec0c979c6673800280d9e99626319
lssch <ls.schwarz@outlook.com>
2023-10-21 15:10:03 +0200

    Proof of concept
    Implemented the basic functionality to positioning the servo based on the standard protocoll.

--- Servo.cpp
+++ Servo.cpp
@@ -3,10 +3,19 @@
 //
 
 #include "Servo.h"
+#include "limits.h"
+#include <iostream>
 
 
-Servo::Servo(TIM_HandleTypeDef *htim_, uint8_t channel_) {
+Servo::Servo(TIM_HandleTypeDef *htim_, uint8_t channel_, uint8_t min_angle, uint8_t max_angle) {
   htim = htim_;
   channel = channel_;
+  steering_limits.min_angle = min_angle;
+  steering_limits.max_angle = max_angle;
 }
 
+void Servo::angle(float steering_angle) {
+  htim->Instance->CCR1 = (steering_angle/1000 + 1) * htim->Instance->ARR/20;
+  std::cout << "CCR1: " << htim->Instance->CCR1 << std::endl;
+  HAL_TIM_PWM_Start(htim, channel);
+}
--- Servo.h
+++ Servo.h
@@ -9,11 +9,15 @@
 
 class Servo {
 public:
-  Servo(TIM_HandleTypeDef *htim_, uint8_t channel_);
-  //void config(parameter);
+  Servo(TIM_HandleTypeDef *htim_, uint8_t channel_, uint8_t min_angle, uint8_t max_angle);
+  void angle(float steering_angle);
 private:
   TIM_HandleTypeDef *htim;
   uint8_t channel;
+  struct {
+    uint8_t min_angle;
+    uint8_t max_angle;
+  } steering_limits;
 };
 
 #endif //STM32_SERVO_SERVO_H
